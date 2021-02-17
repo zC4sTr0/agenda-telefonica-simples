c7af9f29c7aec23f4036792cf226bb278949d63c
Gabriel Castro <38432614+zC4sTr0@users.noreply.github.com>
2021-02-17 08:10:48 -0300

    setting a more interesting mysql user/pass

--- util/database.js
+++ util/database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: "127.0.0.1",
-    user: "root",
-    password: "",
+    user: "davinTI",
+    password: "alwayslearning",
     database: "agenda"
 });
 
